056da0a2037f7844cd87baaa5b364ea5b11efb9a
Gabriel Volpe <volpegabriel@gmail.com>
2020-10-20 11:03:58 +0200

    lenses 3.3: limitations

--- src/Lenses.hs
+++ src/Lenses.hs
@@ -56,3 +56,26 @@ lenses_3_3_1 = view numCrew myShip
 lenses_3_3_2_1 = [ "wand", "book", "potions" ] -- generated lenses
 lenses_3_3_2_2 = "gazor :: Lens' Chumble Spuzz"
 lenses_3_3_2_3 = "move the makeLenses call right below the record definition"
+
+lenses_3_4_1 :: Lens' (a, b, c) b
+lenses_3_4_1 = _2
+
+lenses_3_4_2 :: Lens' (Maybe a) a
+lenses_3_4_2 = undefined -- Not possible, it's a Prism: _Just
+
+lenses_3_4_3 :: Lens' (Either a b) a
+lenses_3_4_3 = undefined -- Not possible, also a Prism: _Left. If we have `Either a a` instead, we could use `chosen`.
+
+-- focus on third element of a list
+lenses_3_4_4 :: Lens' [a] a
+lenses_3_4_4 = undefined -- Not possible, it's a Traversal.
+
+-- conditional: if (_1) then _2 else _3
+lenses_3_4_5 :: Lens' (Bool, a, a) a
+lenses_3_4_5 = lens (\(b, a1, a2) -> if b then a1 else a2) (\(b, a1, a2) a' -> if b then (b, a', a2) else (b, a1, a'))
+
+data Err = ReallyBadError { _msg :: String } | ExitCode { _code :: Int }
+makeLenses ''Err
+
+lenses_3_4_6_msg :: Lens' Err String
+lenses_3_4_6_msg = undefined -- Not possible. Needs a Prism to focus on `ReallyBadError` and then apply the `msg` lens.
